de88459b9dc945a35d64fe0539df529621a96c8a
jesuslc91 <jesuslc.91@gmail.com>
2017-05-19 19:45:56 +0200

    count pauses, resumes and times between pause and resume events

--- tracker.js
+++ tracker.js
@@ -3,6 +3,11 @@
 			this.videoPlayer = videoPlayer;
 			this.pausedCount = 0;
 			this.resumeCount = 0;
+			this.alreadyStarted = false;
+			this.lastPercentSeen = 0;
+			this.secondsPaused = 0;
+			
+			this.startTimeStamp = Date.now();
 			this._subscribeEvents();
 		}
 		
@@ -13,15 +18,35 @@
 		    this.videoPlayer.onpause = () => {
 				this._playerPaused();
 			};
+			this.videoPlayer.onended = () => {
+				this._playerEnded();
+			};
 		}
 
 		_playerResumed() {
 			this.resumeCount++;
-			console.log('play');
+			if(!this.alreadyStarted) {
+				this.alreadyStarted = true;
+				// send http video start
+			} else {
+				// send http video resume
+			}
+			this.lastIntervalStart = this.videoPlayer.currentTime;
+			this.secondsPaused = (Date.now() - this.startTimeStamp) / 1000;
+			console.log( `Paused ${this.secondsPaused} seconds.` ); // to page element
+			console.log( `Resumed ${this.resumeCount} times.` ); // to page element
 		}
 		
 		_playerPaused() {
 			this.pausedCount++;
-			console.log('pause');
+			this.startTimeStamp = Date.now();
+			const lastInterval = this.videoPlayer.currentTime - this.lastIntervalStart;
+			this.lastPercentSeen = (lastInterval / this.videoPlayer.duration) * 100;
+			console.log( `${this.lastPercentSeen}% of the video seen.`); // to page element
+			console.log( `Paused ${this.pausedCount} times.` ); // to page element
+		}
+		
+		_playerEnded() {
+			// send http video end
 		}
 	}
