7f9a53f3a1fb7fdb8991d6bcdaea7f4d1280dd22
jesuslc91 <jesuslc.91@gmail.com>
2017-05-19 20:43:57 +0200

    http requests

--- tracker.js
+++ tracker.js
@@ -1,6 +1,8 @@
 	class VideoTracker {
 		constructor(videoPlayer){
+			this.requestURL = 'https://httpbin.org/post';
 			this.videoPlayer = videoPlayer;
+			
 			this.pausedCount = 0;
 			this.resumeCount = 0;
 			this.alreadyStarted = false;
@@ -25,16 +27,20 @@
 
 		_playerResumed() {
 			this.resumeCount++;
-			if(!this.alreadyStarted) {
-				this.alreadyStarted = true;
-				// send http video start
-			} else {
-				// send http video resume
-			}
+			
 			this.lastIntervalStart = this.videoPlayer.currentTime;
 			this.secondsPaused = (Date.now() - this.startTimeStamp) / 1000;
 			console.log( `Paused ${this.secondsPaused} seconds.` ); // to page element
-			console.log( `Resumed ${this.resumeCount} times.` ); // to page element
+			console.log( `Resumed ${this.resumeCount} times.` ); // to page element	
+
+			if(!this.alreadyStarted) {
+				this.alreadyStarted = true;
+				// send http video start
+				this.sendDataStart(this.secondsPaused);
+			} else {
+				// send http video resume
+				this.sendDataResume(this.resumeCount,this.secondsPaused);
+			}
 		}
 		
 		_playerPaused() {
@@ -48,5 +54,35 @@
 		
 		_playerEnded() {
 			// send http video end
+			this.sendDataEnd(this.resumeCount,this.pausedCount);
+		}
+		
+		sendDataStart(pausedTime) {
+			var data = new FormData();
+			data.append('Started','True');
+			data.append('LoadedToPlayTime', pausedTime);
+			this.sendRequest(data);
+		}
+		
+		sendDataResume(resumedCount, secondsPaused) {
+			var data = new FormData();
+			data.append('Resumed','True');
+			data.append('ResumedCount', resumedCount);
+			data.append('SecondsPaused', secondsPaused);
+			this.sendRequest(data);
+		}
+		
+		sendDataEnd(resumedCount, pausedCount) {
+			var data = new FormData();
+			data.append('Ended','True');
+			data.append('ResumedCount', resumedCount);
+			data.append('PausedCount', pausedCount);
+			this.sendRequest(data);
+		}
+		
+		sendRequest(dataToSend){
+			var request = new XMLHttpRequest();
+			request.open('POST', this.requestURL, true); // async
+			request.send(dataToSend);
 		}
 	}
