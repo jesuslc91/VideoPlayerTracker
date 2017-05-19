12c2797e1f6f333d16319a4b0b02203e14294252
jesuslc91 <jesuslc.91@gmail.com>
2017-05-19 17:59:15 +0200

    event subscription

--- tracker.js
+++ tracker.js
@@ -1,5 +1,27 @@
 	class VideoTracker {
-		constructor(){
+		constructor(videoPlayer){
+			this.videoPlayer = videoPlayer;
+			this.pausedCount = 0;
+			this.resumeCount = 0;
+			this._subscribeEvents();
+		}
+		
+		_subscribeEvents() {
+			this.videoPlayer.onplay = () => {
+				this._playerResumed();
+			};
+		    this.videoPlayer.onpause = () => {
+				this._playerPaused();
+			};
+		}
 
+		_playerResumed() {
+			this.resumeCount++;
+			console.log('play');
+		}
+		
+		_playerPaused() {
+			this.pausedCount++;
+			console.log('pause');
 		}
 	}
