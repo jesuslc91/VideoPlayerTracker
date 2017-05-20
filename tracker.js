8c79eecf3f87a1016b0d8b529b48f1ecbc67151a
jesuslc91 <jesuslc.91@gmail.com>
2017-05-20 12:14:37 +0200

    div to show results, code clean

--- tracker.js
+++ tracker.js
@@ -1,15 +1,19 @@
 	class VideoTracker {
-		constructor(videoPlayer){
-			this.requestURL = 'https://httpbin.org/post';
+		constructor(videoPlayer, requestURL, outputText = undefined){
+			// The player to track
 			this.videoPlayer = videoPlayer;
+			// The URL to send data
+			this.requestURL = requestURL;
+			// (optional) the div to show events
+			this.outputText = outputText;
 			
 			this.pausedCount = 0;
 			this.resumeCount = 0;
 			this.alreadyStarted = false;
 			this.lastPercentSeen = 0;
 			this.secondsPaused = 0;
-			
 			this.startTimeStamp = Date.now();
+
 			this._subscribeEvents();
 		}
 		
@@ -27,12 +31,17 @@
 
 		_playerResumed() {
 			this.resumeCount++;
-			
 			this.lastIntervalStart = this.videoPlayer.currentTime;
 			this.secondsPaused = (Date.now() - this.startTimeStamp) / 1000;
-			console.log( `Paused ${this.secondsPaused} seconds.` ); // to page element
-			console.log( `Resumed ${this.resumeCount} times.` ); // to page element	
-
+			// text to console
+			console.log( `Paused ${this.secondsPaused} seconds.` );
+			console.log( `Resumed ${this.resumeCount} times.` );
+			// if we have a div, text also shown there
+			if(this.outputText){
+				this.outputText.innerHTML = `${this.outputText.innerHTML}<br> Paused ${this.secondsPaused} seconds.`;
+				this.outputText.innerHTML = `${this.outputText.innerHTML}<br> Resumed ${this.resumeCount} times.`;
+			}
+			
 			if(!this.alreadyStarted) {
 				this.alreadyStarted = true;
 				// send http video start
@@ -48,8 +57,14 @@
 			this.startTimeStamp = Date.now();
 			const lastInterval = this.videoPlayer.currentTime - this.lastIntervalStart;
 			this.lastPercentSeen = (lastInterval / this.videoPlayer.duration) * 100;
-			console.log( `${this.lastPercentSeen}% of the video seen.`); // to page element
-			console.log( `Paused ${this.pausedCount} times.` ); // to page element
+			// text to console
+			console.log( `${this.lastPercentSeen}% of the video seen.`);
+			console.log( `Paused ${this.pausedCount} times.` );
+			// if we have a div, text also shown there
+			if(this.outputText){
+				this.outputText.innerHTML = `${this.outputText.innerHTML}<br> ${this.lastPercentSeen}% of the video seen.`;
+				this.outputText.innerHTML = `${this.outputText.innerHTML}<br> Paused ${this.pausedCount} times.`;
+			}
 		}
 		
 		_playerEnded() {
@@ -57,6 +72,7 @@
 			this.sendDataEnd(this.resumeCount,this.pausedCount);
 		}
 		
+		// http requests, data object creators for each case
 		sendDataStart(pausedTime) {
 			var data = new FormData();
 			data.append('Started','True');
@@ -80,6 +96,7 @@
 			this.sendRequest(data);
 		}
 		
+		// http request send
 		sendRequest(dataToSend){
 			var request = new XMLHttpRequest();
 			request.open('POST', this.requestURL, true); // async
